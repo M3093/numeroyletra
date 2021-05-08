bdc4a671ffb5a1dde3914c82c3f44d69b996d50f
MariaBadilla <>
2021-05-08 18:10:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string unidades[] = 
+    {"cero", "uno", "dos" ,"tres" ,"cuatro" ,"cinco" ,
+            "seis" ,"siete" ,"ocho" ,"nueve","diez"};
+    string diferentes[] = 
+    {"once", "doce","trece","catorce", "quince",
+              "diezciseis", "diecisiete", "dieciocho", "diecinueve"};
+    string decenas[] = 
+    {"veinte", "treinta","cuarenta","cincuenta", "sesenta",
+           "setenta", "ochenta", "noventa"};
+  string centenas[] = 
+    {"cien", "dosientos","trecientos","cuatrocisnetos", "quinientos",
+           "seisientos", "setecientos", "ochosientos", "novecientos"};
+    
+    cout << "Ingrese un numero entre 0-999: ";
+    int num;
+    cin >> num;
+
+    if(num>=0 && num<11)
+        cout << endl << unidades[num];
+    else if(num<20)
+        cout << endl << diferentes[num-11];
+    else if(num<100){
+        int unid = num % 10;
+        int dec = num/10;
+        if(unid == 0)
+            cout << endl << decenas[dec-2] ;
+        else
+            cout << endl << decenas[dec-2] <<" y " <<unidades[unid];
+      }
+
+       if(num>=0 && num<111)
+        cout << endl << decenas[num];
+    else if(num<100)
+        cout << endl << decenas[num-111];
+    else if(num<1000){
+        int unid = num % 10;
+        int dec = num % 100;
+        int cent = num/100;
+        if(dec == 0)
+            cout << endl << centenas[cent-2] ;
+        else
+            cout << endl << centenas[cent-2] << decenas[dec-2] <<" y " <<unidades[unid]; 
+    }
+    else
+        cout << "El numero debe ser menor a 1000";
+
+    return 0;
+}
