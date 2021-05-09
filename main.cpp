81ba2de19133ab4e926cacca22ba249be910e200
Maria Badilla <>
2021-05-09 16:24:05 +0000

    Comentarios

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
-//Programa de pasar números a letras 
+//Programa de pasar números a letras
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-int main() 
+// Declaración de variables
+  int num; 
+  int unid;
+  int dec;
+  int cent; 
+
+int main() //Cadena de string, es una secuencia de  caracteres
 {
     string unidades[] = 
     {"cero", "uno", "dos" ,"tres" ,"cuatro" ,"cinco" ,
-            "seis" ,"siete" ,"ocho" ,"nueve","diez"};
+     "seis" ,"siete" ,"ocho" ,"nueve","diez"};
+
     string diferentes[] = 
     {"once", "doce","trece","catorce", "quince",
-              "diezciseis", "diecisiete", "dieciocho", "diecinueve"};
+    "diezciseis", "diecisiete", "dieciocho", "diecinueve"};
+
     string decenas[] = 
     {"veinte", "treinta","cuarenta","cincuenta", "sesenta",
-           "setenta", "ochenta", "noventa"};
-  string centenas[] = 
-    {"cien", "dosientos","trecientos","cuatrocisnetos", "quinientos",
-           "seisientos", "setecientos", "ochosientos", "novecientos"};
+    "setenta", "ochenta", "noventa"};
+
+    string centenas[] = 
+    {"cien","ciento", "doscientos","trescientos",     "cuatrocientos", "quinientos","seiscientos", "setecientos", "ochocientos", "novecientos"};
     
     cout << "Ingrese un numero entre 0-999: ";
     int num;
@@ -36,18 +44,18 @@ int main()
             cout << endl << decenas[dec-2] <<" y " <<unidades[unid];
       }
 
-       if(num>=0 && num<111)
+    if(num>=0 && num<111)
         cout << endl << decenas[num];
     else if(num<100)
         cout << endl << decenas[num-111];
     else if(num<1000){
         int unid = num % 10;
-        int dec = num % 100;
+        int dec = num % 10;
         int cent = num/100;
         if(dec == 0)
             cout << endl << centenas[cent-2] ;
         else
-            cout << endl << centenas[cent-2] << decenas[dec-2] <<" y " <<unidades[unid]; 
+            cout << endl << centenas[cent-2] << " " << decenas[dec-2] <<" y " <<unidades[unid]; 
     }
     else
         cout << "El numero debe ser menor a 1000";
